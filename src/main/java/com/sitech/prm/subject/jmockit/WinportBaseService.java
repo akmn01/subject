2253a24ee941b2aaba0dc07813d77822f424515f
sunyz <215240441@qq.com>
2016-09-20 21:05:23 +0800

    Revert "firstVersion"
    This reverts commit 6a4866f96143cef13c76609c3488ae11b489bc43.

--- src/main/java/com/sitech/prm/subject/jmockit/WinportBaseService.java
+++
@@ -1,7 +0,0 @@
-package com.sitech.prm.subject.jmockit;
-
-public class WinportBaseService {  
-    public long getPostedOfferCounts(String memberId) {  
-        return 1000;  
-    }  
-} 
--- src/main/java/com/sitech/prm/subject/jmockit/WinportUrlService.java
+++
@@ -1,7 +0,0 @@
-package com.sitech.prm.subject.jmockit;
-
-public interface WinportUrlService {  
-    boolean hasWinport(String memberId);  
-  
-    String getWinportUrlThrowException(String memberId);  
-} 
